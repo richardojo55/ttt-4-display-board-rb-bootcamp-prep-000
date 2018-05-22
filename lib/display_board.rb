885568c0126a3feceea4ef5a28a5e56c10750b0b
Olawale Ojo <richardojo9@gmail.com>
2018-05-22 18:31:11 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,4 @@
-board = ["X", "O", " ", " ", "O", " ", "O", "X", " "]
+board = ["O", "X", " ", " ", "X", " ", "X, "X", " "]
 
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
